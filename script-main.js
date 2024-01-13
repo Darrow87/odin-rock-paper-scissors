63c080e7bd9b42615b065d09730c26f7e1954cab
Alex Forger <alexforgerii@gmail.com>
2024-01-13 10:07:45 -1000

    write function that determines winner between computer and player choices

--- script-main.js
+++ script-main.js
@@ -2,3 +2,33 @@ function getComputerChoice() {
     let choices = ['Rock', 'Paper', 'Scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function playRound(playerChoice, computerChoice) {
+    switch(playerChoice) {
+        case 'rock':
+            if (computerChoice === 'scissors') {
+                return 'Player wins!';
+            } else if (computerChoice === 'paper') {
+                return 'Computer wins!';
+            }
+            break;
+        case 'paper':
+            if (computerChoice === 'rock') {
+                return 'Player wins!';
+            } else if (computerChoice === 'scissors') {
+                return 'Computer wins!';
+            }
+            break;
+        case 'scissors':
+            if (computerChoice === 'paper') {
+                return 'Player wins!';
+            } else if (computerChoice === 'rock') {
+                return 'Computer wins!';
+            }
+            break;
+        default:
+            return 'Invalid choice';
+    }
+    return 'It\'s a tie!';
+}
+
